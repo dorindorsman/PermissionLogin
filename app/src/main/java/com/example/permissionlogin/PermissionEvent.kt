639a7b4df298417e89fe590788d4a70fd43e4e20
Dorin Dorsman <dorin.dorsman@intel.com>
2022-11-02 01:32:46 +0200

    setup View ViewModel Event

--- app/src/main/java/com/example/permissionlogin/PermissionEvent.kt
+++ app/src/main/java/com/example/permissionlogin/PermissionEvent.kt
@@ -1,8 +1,6 @@
 package com.example.permissionlogin
 
 sealed class PermissionEvent {
-
     class UpdatePasswordTyped(val p:String) : PermissionEvent()
     class LoginClicked(val p:String) : PermissionEvent()
-
 }
